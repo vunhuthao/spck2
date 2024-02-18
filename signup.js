c46aba715a14a20e8a25baf2d476dae03bf4ab54
vunhuthao <142017699+vunhuthao@users.noreply.github.com>
2024-02-18 11:32:37 +0700

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth,
+    createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyAQXm4jPJlv05o4qJqc41DWDEapmddo9GE",
+  authDomain: "spck-3848c.firebaseapp.com",
+  projectId: "spck-3848c",
+  storageBucket: "spck-3848c.appspot.com",
+  messagingSenderId: "1082942817239",
+  appId: "1:1082942817239:web:c1693434e3dd51eecd0192",
+  measurementId: "G-6E37XN59H3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const signupB = document.querySelector("#signupB");
+
+signupB.addEventListener("click", () => {
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#pass").value;
+
+
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    alert("OK");
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert("lỗi");
+  });
+
+})
+
